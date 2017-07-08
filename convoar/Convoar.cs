f8bd2b38533e24dff9c4573ad2b3e2e92cb68a84
Robert Adams <misterblue@misterblue.com>
2017-07-08 11:46:15 -0700

    Reorganize main code so ConvOAR operations are callable programmatically. Change command line invocation to use new programatic functions.

--- convoar/Convoar.cs
+++ convoar/Convoar.cs
@@ -21,6 +21,8 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
+using OpenSim.Services.Interfaces;
+
 
 namespace org.herbal3d.convoar {
 
@@ -67,10 +69,14 @@ convoar
             return;
         }
 
+        // If run from the command line, create instance and call 'Start' with args.
+        // If run programmatically, create instance and call 'Start' with parameters.
         public void Start(string[] args) {
             Globals = new GlobalContext(new ConvoarParams(), new LoggerLog4Net());
             Globals.stats = new ConvoarStats();
 
+            // 'ConvoarParams' initializes to default values.
+            // Over ride default values with command line parameters.
             try {
                 Globals.parms.MergeCommandLine(args, null, "InputOAR");
             }
@@ -94,60 +100,58 @@ convoar
                 _outputDir = "./out";
                 Globals.log.DebugFormat("Output directory defaulting to {0}", _outputDir);
             }
-           
+
+            // Base asset storage system -- 'MemAssetService' is in-memory storage
             using (MemAssetService memAssetService = new MemAssetService()) {
 
+                // Asset cache and fetching wrapper code -- where all the mesh, material, and instance
+                //    information is stored for later processing.
                 using (IAssetFetcher assetFetcher = new OSAssetFetcher(memAssetService)) {
 
-                    BConverterOS converter = new BConverterOS();
-
                     try {
-                        converter.ConvertOarToScene(memAssetService, assetFetcher)
-                            .Catch(e => {
-                                Globals.log.ErrorFormat("{0} Exception converting scene: {1}", _logHeader, e);
-                            })
-                            .Then(bScene => {
-                                Globals.contextName = bScene.name;
+                        LoadOAR(memAssetService, assetFetcher, bScene => {
+                            Globals.contextName = bScene.name;
 
-                                Globals.log.DebugFormat("{0} Scene created. name={1}, instances={2}",
-                                    _logHeader, bScene.name, bScene.instances.Count);
-                                Globals.log.DebugFormat("{0}    num assetFetcher.images={1}", _logHeader, assetFetcher.Images.Count);
-                                Globals.log.DebugFormat("{0}    num assetFetcher.materials={1}", _logHeader, assetFetcher.Materials.Count);
-                                Globals.log.DebugFormat("{0}    num assetFetcher.meshes={1}", _logHeader, assetFetcher.Meshes.Count);
-                                Globals.log.DebugFormat("{0}    num assetFetcher.renderables={1}", _logHeader, assetFetcher.Renderables.Count);
+                            Globals.log.DebugFormat("{0} Scene created. name={1}, instances={2}",
+                                _logHeader, bScene.name, bScene.instances.Count);
+                            Globals.log.DebugFormat("{0}    num assetFetcher.images={1}", _logHeader, assetFetcher.Images.Count);
+                            Globals.log.DebugFormat("{0}    num assetFetcher.materials={1}", _logHeader, assetFetcher.Materials.Count);
+                            Globals.log.DebugFormat("{0}    num assetFetcher.meshes={1}", _logHeader, assetFetcher.Meshes.Count);
+                            Globals.log.DebugFormat("{0}    num assetFetcher.renderables={1}", _logHeader, assetFetcher.Renderables.Count);
 
-                                // Perform any optimizations on the scene and its instances
+                            // Perform any optimizations on the scene and its instances
 
-                                // Output the transformed scene
-                                Gltf gltf = new Gltf();
-                                gltf.LoadScene(bScene, assetFetcher);
+                            // Output the transformed scene
+                            Gltf gltf = new Gltf();
+                            gltf.LoadScene(bScene, assetFetcher);
 
-                                Globals.log.DebugFormat("{0}   num Gltf.nodes={1}", _logHeader, gltf.nodes.Count);
-                                Globals.log.DebugFormat("{0}   num Gltf.meshes={1}", _logHeader, gltf.meshes.Count);
-                                Globals.log.DebugFormat("{0}   num Gltf.materials={1}", _logHeader, gltf.materials.Count);
-                                Globals.log.DebugFormat("{0}   num Gltf.images={1}", _logHeader, gltf.images.Count);
-                                Globals.log.DebugFormat("{0}   num Gltf.accessor={1}", _logHeader, gltf.accessors.Count);
-                                Globals.log.DebugFormat("{0}   num Gltf.buffers={1}", _logHeader, gltf.buffers.Count);
-                                Globals.log.DebugFormat("{0}   num Gltf.bufferViews={1}", _logHeader, gltf.bufferViews.Count);
+                            Globals.log.DebugFormat("{0}   num Gltf.nodes={1}", _logHeader, gltf.nodes.Count);
+                            Globals.log.DebugFormat("{0}   num Gltf.meshes={1}", _logHeader, gltf.meshes.Count);
+                            Globals.log.DebugFormat("{0}   num Gltf.materials={1}", _logHeader, gltf.materials.Count);
+                            Globals.log.DebugFormat("{0}   num Gltf.images={1}", _logHeader, gltf.images.Count);
+                            Globals.log.DebugFormat("{0}   num Gltf.accessor={1}", _logHeader, gltf.accessors.Count);
+                            Globals.log.DebugFormat("{0}   num Gltf.buffers={1}", _logHeader, gltf.buffers.Count);
+                            Globals.log.DebugFormat("{0}   num Gltf.bufferViews={1}", _logHeader, gltf.bufferViews.Count);
 
-                                PersistRules.ResolveAndCreateDir(ConvOAR.Globals.parms.GltfTargetDir);
-                                string gltfFilename = PersistRules.CreateFilename(PersistRules.AssetTypeGltf, bScene.name, "");
+                            PersistRules.ResolveAndCreateDir(ConvOAR.Globals.parms.GltfTargetDir);
+                            string gltfFilename = PersistRules.CreateFilename(PersistRules.AssetTypeGltf, bScene.name, "");
 
-                                using (StreamWriter outt = File.CreateText(gltfFilename)) {
-                                    gltf.ToJSON(outt);
-                                }
-                                gltf.WriteImages();
-                                gltf.WriteBinaryFiles();
+                            using (StreamWriter outt = File.CreateText(gltfFilename))
+                            {
+                                gltf.ToJSON(outt);
                             }
-                        );
+                            gltf.WriteBinaryFiles();
+
+                            gltf.WriteImages();
+
+                        });
                     }
                     catch (Exception e) {
                         Globals.log.ErrorFormat("{0} Global exception converting scene: {1}", _logHeader, e);
+                        // A common error is not having all the DLLs for OpenSimulator. Print out what's missing.
                         ReflectionTypeLoadException refE = e as ReflectionTypeLoadException;
-                        if (refE != null)
-                        {
-                            foreach (var ee in refE.LoaderExceptions)
-                            {
+                        if (refE != null) {
+                            foreach (var ee in refE.LoaderExceptions) {
                                 Globals.log.ErrorFormat("{0} reference exception: {1}", _logHeader, ee);
                             }
                         }
@@ -155,5 +159,33 @@ convoar
                 }
             }
         }
+
+        // Initialization if using ConvOAR programmatically.
+        public void Start(ConvoarParams pParameters, Logger pLogger) {
+            Globals = new GlobalContext(pParameters, pLogger);
+            Globals.stats = new ConvoarStats();
+        }
+
+        // Load the OARfile specified in Globals.params.InputOAR.
+        // Parameters are in 'ConvOAR.Globals.params'.
+        // For the moment, the OAR file must be specified with a string because of how OpenSimulator
+        //     processes the input files. Note that the filename can be an 'http:' type URL.
+        public delegate void BSceneLoadedCallback(BScene loadedScene);
+        public bool LoadOAR(IAssetService assetService, IAssetFetcher assetFetcher, BSceneLoadedCallback loadedCallback) {
+            bool ret = false;
+
+            BConverterOS converter = new BConverterOS();
+
+            converter.ConvertOarToScene(assetService, assetFetcher)
+                .Catch(e => {
+                    throw (e);
+                })
+                .Then(bScene => {
+                    loadedCallback(bScene);
+                });
+
+            return ret;
+        }
+
     }
 }
