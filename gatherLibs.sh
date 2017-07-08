c8732ab0851a973458f1834adc863b13bd305310
Robert Adams <misterblue@misterblue.com>
2017-07-07 22:22:33 -0700

    Move all the dependent libraries into the libs/ directory. Script 'gatherLibs.sh' copies libs from other repositories to libs. Update convoar.csproj to point to libraries in local repository. The only libs not handled are the openjpeg ones.

---
+++ gatherLibs.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+OPENSIM=../opensim-github/bin
+LIBOMV=../libopenmetaverse/bin
+
+# Copy the dll file and the PDB file if it exists
+function GetLib() {
+    cp "$1/$2" libs
+    pdbfile=$1/${2%.dll}.pdb
+    if [[ -e "$pdbfile" ]] ; then
+        cp "$pdbfile" libs
+    fi
+}
+
+GetLib "$OPENSIM" "OpenSim.Data.Null.dll"
+GetLib "$OPENSIM" "OpenSim.Capabilities.dll"
+GetLib "$OPENSIM" "OpenSim.Framework.dll"
+GetLib "$OPENSIM" "OpenSim.Framework.Monitoring.dll"
+GetLib "$OPENSIM" "OpenSim.Framework.Serialization.dll"
+GetLib "$OPENSIM" "OpenSim.Framework.Servers.HttpServer.dll"
+GetLib "$OPENSIM" "OpenSim.Services.Interfaces.dll"
+GetLib "$OPENSIM" "OpenSim.Services.Connectors.dll"
+GetLib "$OPENSIM" "OpenSim.Region.CoreModules.dll"
+GetLib "$OPENSIM" "OpenSim.Region.Framework.dll"
+GetLib "$OPENSIM" "OpenSim.Region.PhysicsModule.BasicPhysics.dll"
+GetLib "$OPENSIM" "OpenSim.Region.PhysicsModules.SharedBase.dll"
+GetLib "$OPENSIM" "OpenSim.Tests.Common.dll"
+
+GetLib "$OPENSIM" "log4net.dll"
+GetLib "$OPENSIM" "Nini.dll"
+GetLib "$OPENSIM" "nunit.framework.dll"
+GetLib "$OPENSIM" "Mono.Addins.dll"
+
+GetLib "$LIBOMV" "OpenMetaverse.dll"
+GetLib "$LIBOMV" "OpenMetaverse.dll.config"
+GetLib "$LIBOMV" "OpenMetaverse.XML"
+GetLib "$LIBOMV" "OpenMetaverseTypes.dll"
+GetLib "$LIBOMV" "OpenMetaverseTypes.XML"
+GetLib "$LIBOMV" "OpenMetaverse.StructuredData.dll"
+GetLib "$LIBOMV" "OpenMetaverse.StructuredData.XML"
+GetLib "$LIBOMV" "OpenMetaverse.Rendering.Meshmerizer.dll"
+
