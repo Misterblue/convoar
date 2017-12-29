707476878a8d44e464d3ab9a7202773ab7abb011
Robert Adams <misterblue@misterblue.com>
2017-12-28 18:56:37 -0800

    Parameterize the run.sh script.

--- test/run.sh
+++ test/run.sh
@@ -3,23 +3,24 @@ mkdir -p convoar
 # format=collada
 format=gltf2
 
-cd convoar
-../../convoar/bin/Debug/convoar.exe --exportFormat $format ../Atropia_12.oar
-exit
-../../convoar/bin/Debug/convoar.exe --exportFormat $format ../testtest88.oar
+CONVOAR=../../convoar/bin/Debug/convoar.exe
 
-../../convoar/bin/Debug/convoar.exe --exportFormat $format ../Atropia_00.oar
-../../convoar/bin/Debug/convoar.exe --exportFormat $format ../Atropia_01.oar
-../../convoar/bin/Debug/convoar.exe --exportFormat $format ../Atropia_02.oar
-../../convoar/bin/Debug/convoar.exe --exportFormat $format ../Atropia_10.oar
-../../convoar/bin/Debug/convoar.exe --exportFormat $format ../Atropia_11.oar
-../../convoar/bin/Debug/convoar.exe --exportFormat $format ../Atropia_12.oar
-../../convoar/bin/Debug/convoar.exe --exportFormat $format ../Atropia_20.oar
-../../convoar/bin/Debug/convoar.exe --exportFormat $format ../Atropia_21.oar
-../../convoar/bin/Debug/convoar.exe --exportFormat $format ../Atropia_22.oar
-../../convoar/bin/Debug/convoar.exe --exportFormat $format ../IMAOutpostAlphaForest.oar
-../../convoar/bin/Debug/convoar.exe --exportFormat $format ../IMAOutpostAlphaTerrain.oar
-../../convoar/bin/Debug/convoar.exe --exportFormat $format ../Region-3dworlds-20170604.oar
+cd convoar
+$CONVOAR --exportFormat $format ../testtest88.oar
+exit
+
+$CONVOAR --exportFormat $format ../Atropia_00.oar
+$CONVOAR --exportFormat $format ../Atropia_01.oar
+$CONVOAR --exportFormat $format ../Atropia_02.oar
+$CONVOAR --exportFormat $format ../Atropia_10.oar
+$CONVOAR --exportFormat $format ../Atropia_11.oar
+$CONVOAR --exportFormat $format ../Atropia_12.oar
+$CONVOAR --exportFormat $format ../Atropia_20.oar
+$CONVOAR --exportFormat $format ../Atropia_21.oar
+$CONVOAR --exportFormat $format ../Atropia_22.oar
+$CONVOAR --exportFormat $format ../IMAOutpostAlphaForest.oar
+$CONVOAR --exportFormat $format ../IMAOutpostAlphaTerrain.oar
+$CONVOAR --exportFormat $format ../Region-3dworlds-20170604.oar
 
 rsync -r ../convoar basil@nyxx:basil-git/Basiljs/
 exit
