2679003365a903e617e39ae2dc4c97870850f880
Robert Adams <misterblue@misterblue.com>
2018-07-10 15:16:08 -0700

    oars: nearly final version of oars display page.

--- oars/oars.js
+++ oars/oars.js
@@ -60,35 +60,45 @@ function ErrorLog(msg) {
 function BuildTable(data) {
     let rows = [];
 
+    // Create row of headers
     let headers = [];
     tableColumns.forEach(col => {
-        headers.push(makeHeader(makeText(col)));
+        headers.push(makeHeader(col));
     });
     rows.push(makeRow(headers));
 
+    // For each OAR file, create row of description and converted forms
     Object.keys(data).forEach( oar => {
         let cols = [];
         let firstData = [];
-        firstData.push(makeDiv(makeText(oar), 'c-oarName'));
+        firstData.push(makeDiv(oar, 'c-oarName'));
+        if (data[oar].oar) {
+            firstData.push(makeLink(oarURL + oar + '/' + data[oar].oar, makeButtonSmall('OAR')));
+        }
         if (data[oar].image) {
             firstData.push(makeImage(oarURL + oar + '/' + data[oar].image, 'c-oarImage'));
         }
         cols.push(makeData(firstData, 'c-col-name'));
 
-        cols.push(makeData(makeText('desc', 'c-col-desc')));
+        if (data[oar].desc) {
+            cols.push(makeData(makeText(data[oar].desc, 'c-col-desc')));
+        }
+        else {
+            cols.push(makeData(makeText('.', 'c-col-desc')));
+        }
 
         conversionTypes.forEach( conv => {
-            if (data[oar].types.conv) {
-                cols.push(makeDataSelection(data[oar].types.conv, conv, oar));
+            if (data[oar].types[conv]) {
+                cols.push(makeDataSelection(data[oar].types[conv], conv, oar));
             }
             else {
-                cols.push(makeData(makeText('.')));
+                cols.push(makeData('.'));
             }
         });
 
         rows.push(makeRow(cols));
     });
-    $('#c-tableplace').empty().append(makeTable(rows));
+    $('#c-tableplace').empty().append(makeTable(rows, 'c-table'));
 };
 
 function BuildErrorTable(e) {
@@ -97,68 +107,100 @@ function BuildErrorTable(e) {
 
 // Return a table data element containing everything about this type version of the oar
 function makeDataSelection(typeDesc, type, oar) {
-    let pp = [];
+    /*
+    <td class="c-col-selection">
+        <div class="c-selection">
+            <div class="c-viewer">
+                <a href="view" target="_blank">
+                    <button type="button">View</button>
+                </a>
+            </div>
+            <div class="c-downloads">
+                <div>Download:</div>
+                <a href="toTGZ">
+                    <button type="button">TGZ</button>
+                </a>
+                <a href="toZIP">
+                    <button type="button">ZIP</button>
+                </a>
+                
+            </div>
+        </div>
+    </td>
+    */
+    let selectionContents = [];
+
+    let viewDivContents = [];
     if (typeDesc.gltf) {
-        pp.push(makeDiv( makeButtonSmall('View', oar + '/' + type ), 'c-selection-gltf'));
-    };
+        let viewURL = 'https://misterblue.com/justview/justview.html?b=' + oar + '&v=' + type;
+        let viewLink = makeLink(viewURL, makeButton('View'));
+        viewLink.setAttribute('target', '_blank');
+        viewDivContents.push(makeDiv(viewLink, 'c-viewer'));
+    }
+    selectionContents.push(makeDiv(viewDivContents, 'c-viewer'));
+
+    let downloadDivContents = [];
+    downloadDivContents.push(makeDiv('Download:'));
     if (typeDesc.tgz) {
-        pp.push(makeDiv( makeURL(oarURL + oar + '/' + typeDesc.tgz, 'TGZ', 'c-selection-tgz')));
+        downloadDivContents.push(makeLink(oarURL + oar + '/' + type + '/' + typeDesc.tgz, makeButtonSmall('TGZ')));
     }
     if (typeDesc.zip) {
-        pp.push(makeDiv( makeURL(oarURL + oar + '/' + typeDesc.zip, 'ZIP', 'c-selection-zip')));
+        downloadDivContents.push(makeLink(oarURL + oar + '/' + type + '/' + typeDesc.zip, makeButtonSmall('ZIP')));
     }
-    return makeData(pp, 'c-col-selection');
+    selectionContents.push(makeDiv(downloadDivContents, 'c-downloads'));
+
+    return makeData(makeDiv(selectionContents, 'c-selection'), 'c-col-selection');
 };
 
 function makeButton(label, ref) {
-    let but = document.createElement('button');
+    let but = makeElement('button', label, 'button clickable');
     but.setAttribute('type', 'button');
-    but.setAttribute('class', 'button clickable');
-    but.setAttribute('c-op', 'view');
-    but.setAttribute('c-ref', ref);
-    but.appendChild(makeText(label));
+    if (ref) {
+        but.setAttribute('c-op', 'view');
+        but.setAttribute('c-ref', ref);
+    }
     return but;
 }
 
-function makeButtonSmall(label) {
+function makeButtonSmall(label, ref) {
+    let but = makeElement('button', label, 'button-sm clickable');
+    but.setAttribute('type', 'button');
+    if (ref) {
+        but.setAttribute('c-op', 'view');
+        but.setAttribute('c-ref', ref);
+    }
+    return but;
 };
 
-function makeURL(url, text, aClass) {
-    let anchor = document.createElement('a');
+function makeLink(url, contents, aClass) {
+    let anchor = makeElement('a', contents, aClass);
     anchor.setAttribute('href', url);
-    anchor.appendChild(makeText(text));
-    if (aClass) {
-        anchor.setAttribute('class', aClass);
-    }
     return anchor;
 };
 
 function makeTable(contents, aClass) {
-    return makeThing('table', contents, aClass);
+    return makeElement('table', contents, aClass);
 };
 
 function makeRow(contents, aClass) {
-    return makeThing('tr', contents, aClass);
+    return makeElement('tr', contents, aClass);
 };
 
 function makeHeader(contents, aClass) {
-    return makeThing('th', contents, aClass);
+    return makeElement('th', contents, aClass);
 };
 
 function makeData(contents, aClass) {
-    return makeThing('td', contents, aClass);
+    return makeElement('td', contents, aClass);
 };
 
 function makeDiv(contents, aClass) {
-    return makeThing('div', contents, aClass);
+    return makeElement('div', contents, aClass);
 };
 
 function makeImage(src, aClass) {
-    let img = document.createElement('img');
+    let img = makeElement('img', undefined, aClass);
     img.setAttribute('src', src);
-    if (aClass) {
-        img.setAttribute('class', aClass)
-    }
     return img;
 }
 
@@ -167,22 +209,32 @@ function makeText(contents) {
     return tex;
 };
 
-function makeThing(type, contents, aClass) {
-    let thing = document.createElement(type);
+function makeElement(type, contents, aClass) {
+    let elem = document.createElement(type);
     if (aClass) {
-        thing.setAttribute('class', aClass);
+        elem.setAttribute('class', aClass);
     }
     if (contents) {
         if (Array.isArray(contents)) {
             contents.forEach(ent => {
                 if (typeof(ent) != 'undefined') {
-                    thing.appendChild(ent);
+                    if (typeof(contents) === 'string') {
+                        elem.appendChild(makeText(contents));
+                    }
+                    else {
+                        elem.appendChild(ent);
+                    }
                 }
             });
         }
         else {
-            thing.appendChild(contents);
+            if (typeof(contents) === 'string') {
+                elem.appendChild(makeText(contents));
+            }
+            else {
+                elem.appendChild(contents);
+            }
         }
     }
-    return thing;
+    return elem;
 };
